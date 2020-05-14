aaa24231a90f402fe99a0881606f45e9c640d3b9
haiphung1 <haipmph05501@fpt.edu.vn>
2020-05-14 16:47:51 +0700

    Chapter 6: modeling users

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  EMAIL_REGEX = Settings.VALID_EMAIL_REGEX
+
+  validates :name, presence: true, length: {minimum: Settings.validation.min_name, maximum: Settings.validation.max_name}
+  validates :email, presence: true, length: {maximum: Settings.validation.max_email}, uniqueness: true,
+    format: {with: EMAIL_REGEX}
+  
+  has_secure_password
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
-    get "static_pages/home"
+    root "static_pages#home"
+    
     get "static_pages/help"
   end
 end
