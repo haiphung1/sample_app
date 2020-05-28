78e4a6db26cd73dc0ad8da780a6f408be6d0881f
haiphung1 <haipmph05501@fpt.edu.vn>
2020-05-28 16:43:44 +0700

    Chapter 14: Following users

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,4 +4,8 @@ module UserHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag gravatar_url, alt: user.name, class: "gravatar"
   end
+  
+  def unfollow
+    current_user.active_relationships.find_by followed_id: @user.id
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module SampleApp
     I18n.available_locales = [:vi, :en]
     I18n.default_locale = :vi
 
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
